7193c5df39786202a45385fb53cbe67a5f4e93e2
Meghana-sk <meghana.meghanask.sk@gmail.com>
2022-09-08 03:47:17 +0530

    feat: Add context api, draggable feature

--- src/components/draggableBox/DraggableBox.jsx
+++ src/components/draggableBox/DraggableBox.jsx
@@ -1,20 +1,47 @@
 import Draggable from "react-draggable";
+import { useState } from "react";
+import "../../screens/screens-styles.css";
+import "./draggableBox.css";
+import { useBoxPosition } from "../../context/boxPosition-context";
+import { OTHER_POSITION } from "../../variables/types";
 
 const DraggableBox = () => {
+  const [showPosition, setShowPosition] = useState({ x: 0, y: 0 });
+  const { boxPositionState, boxPositionChangeDispatch } = useBoxPosition();
+  const updateBoxPosition = (data) => {
+    console.log(data);
+    boxPositionChangeDispatch({
+      type: OTHER_POSITION,
+      payload: "other",
+      values: { x: data.x, y: data.y },
+    });
+  };
+  console.log(boxPositionState, "--", showPosition);
   return (
     <>
-      <Draggable
-        handle=".handle"
-        defaultPosition={{ x: 0, y: 0 }}
-        position={null}
-        // onDrag={() => {}}
-        // onStop={() => {}}
+      <div
+        className={`drag-box ${
+          boxPositionState.position === "center" ? "center" : "bottom-right"
+        }`}
       >
-        <div>
-          <div className="handle">Drag from here</div>
-          <div>This readme is really dragging on...</div>
-        </div>
-      </Draggable>
+        <Draggable
+          handle=".drag"
+          defaultPosition={showPosition}
+          position={boxPositionState.coordinates}
+          onDrag={(e, data) => updateBoxPosition(data)}
+          onStop={() => setShowPosition(boxPositionState.coordinates)}
+        >
+          <div className="drag drag-container">
+            <div className="drag">
+              {boxPositionState.position === "center" ||
+              boxPositionState.position === "bottom-right"
+                ? boxPositionState.position
+                : "Floating..."}
+            </div>
+            <div>Drag me around...</div>
+          </div>
+        </Draggable>
+      </div>
     </>
   );
 };
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,43 @@
-import { useState, useEffect } from "react";
 import "./header.css";
+import { useBoxPosition } from "../../context/boxPosition-context";
+import { CENTER, BOTTOM_RIGHT } from "../../variables/types";
+
 const Header = () => {
+  const { boxPositionState, boxPositionChangeDispatch } = useBoxPosition();
   const clock = new Date();
   const date = `${clock.getHours()}:${clock.getMinutes()}:${clock.getSeconds()}`;
   return (
     <nav className="header">
       <h3>Position</h3>
       <div>
-        <input type="radio" name="position-center" id="position-center" />
+        <input
+          type="radio"
+          name="position"
+          id="position-center"
+          value="center"
+          checked={boxPositionState.position === "center"}
+          onChange={(e) =>
+            boxPositionChangeDispatch({
+              type: CENTER,
+              payload: e.target.value,
+            })
+          }
+        />
         <label htmlFor="position-center">Center</label>
       </div>
       <div>
         <input
           type="radio"
-          name="position-lower-right"
+          name="position"
           id="position-lower-right"
+          value="bottom-right"
+          checked={boxPositionState.position === "bottom-right"}
+          onChange={(e) =>
+            boxPositionChangeDispatch({
+              type: BOTTOM_RIGHT,
+              payload: e.target.value,
+            })
+          }
         />
         <label htmlFor="position-lower-right">Lower right</label>
       </div>
